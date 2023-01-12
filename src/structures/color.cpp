518cdf966e4d20fd00cfa7ae9646ad237a8d5d13
Felipe Dowsley <joao.dowsley850@gmail.com>
2023-01-12 09:20:43 -0300

    Format color.cpp within standards

--- src/structures/color.cpp
+++ src/structures/color.cpp
@@ -2,25 +2,30 @@
 
 #include <stdexcept>
 
-Color::Color(float r, float g, float b) {
+Color::Color(float r, float g, float b)
+{
     setRed(r);
     setGreen(g);
     setBlue(b);
 }
 
-float Color::validateColorValue(float val) {
+float Color::validateColorValue(float val)
+{
     if (!(val >= 0.0f && val <= 1.0f)) {
         throw std::invalid_argument("Color value must be between 0.0f and 1.0f range.");
     }
     return val;
 }
 
-void Color::setRed(float val) {
+void Color::setRed(float val)
+{
     this->r = validateColorValue(val);
 }
-void Color::setGreen(float val) {
+void Color::setGreen(float val)
+{
     this->g = validateColorValue(val);
 }
-void Color::setBlue(float val) {
+void Color::setBlue(float val)
+{
     this->b = validateColorValue(val);
 }
